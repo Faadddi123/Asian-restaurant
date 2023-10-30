7784c13c563a06dc29a437ba4774da4d34dd3d0c
siskoweb <sisko4dev@gmail.com>
2023-10-30 14:04:48 +0100

    add active links in navbar

--- js/fadi.js
+++ js/fadi.js
@@ -5,3 +5,22 @@ menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
 
+
+
+
+
+console.log(url)
+
+
+window.addEventListener("load", () => {
+  const links = document.querySelectorAll("#link-navbar");
+  const url = window.location.href;
+  links.forEach((link) => {
+    console.log(link.href)
+    if (url === link.href) {
+      link.classList.add("activeLink");
+    } else {
+      link.classList.remove("activeLink");
+    }
+  });
+})
