94429b3fcbbe6ce5e5cb724da5d3eb196aa941ac
siskoweb <sisko4dev@gmail.com>
2023-11-01 14:30:12 +0100

    add loader to avoid late load in home page and fix url invoice

--- js/loading.js
+++ js/loading.js
@@ -1,10 +1,15 @@
 
 
 document.addEventListener("DOMContentLoaded", async function () {
+    const bodyBome = document.getElementById("body-home");
+
     const loadingOverlay = document.getElementById("loading-overlay");
+
+
 
     // After 2s hide Loader
     setInterval(() => {
         loadingOverlay.style.display = "none";
     }, 2000)
+    bodyBome.classList.remove("hidden")
 });
